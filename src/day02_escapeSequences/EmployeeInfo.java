3822c3013be1d5318ee01d8de76e011c44f360a7
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-07-17 22:35:22 -0500

    Java Programming 02 done

---
+++ src/day02_escapeSequences/EmployeeInfo.java
@@ -0,0 +1,16 @@
+package day02_escapeSequences;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Name:  Zhibek");
+        System.out.println("Gender:  Female");
+        System.out.println("Age: 32");
+        System.out.println("Job Title:  Java Developer");
+        System.out.println("Company:  Apple Inc");
+        System.out.println("Salary: 120,000");
+
+    }
+}
