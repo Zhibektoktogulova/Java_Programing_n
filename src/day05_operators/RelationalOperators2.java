91b6c74536f4d87b80e38afcaa3eec3fe206f81f
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-07-31 16:37:06 -0500

    Relational  Operators 2 is finished

---
+++ src/day05_operators/RelationalOperators2.java
@@ -0,0 +1,46 @@
+package day05_operators;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class RelationalOperators2 {
+
+    public static void main(String[] args) {
+
+        // Equal  ==;
+
+        System.out.println(1000 == 2000);
+
+        System.out.println("Java" == "Java");
+
+        System.out.println("java" == "JAVA");
+
+        System.out.println("Muhtar" == "Good guy"); //
+
+        System.out.println(true == false);
+
+        // Not equal !=
+
+        System.out.println(1000 != 2000); // it's not equal and gives me true
+
+        System.out.println("Java" != "Java"); //it's equal it gives me false
+
+        System.out.println(1000 != 2000);
+
+        System.out.println("Java" != "Java");
+
+        System.out.println("java" != "JAVA");
+
+        System.out.println("Muhtar" != "Good guy"); //
+
+        System.out.println(true != false);
+
+
+
+
+
+
+
+
+
+    }
+}
