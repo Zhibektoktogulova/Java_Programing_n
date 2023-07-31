8561112b23301795b4283500a0b8710585c190d3
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-07-31 11:37:35 -0500

    Arithmetic Operators and Concatenation Addition is done

---
+++ src/day04_concatenation/ArithmeticOperators.java
@@ -0,0 +1,45 @@
+package day04_concatenation;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        System.out.println(100 + 200); //300 int
+        System.out.println(200 - 100);// 100 int
+        System.out.println( 50 / 6); // 8 int
+        System.out.println(50 / 6.0); // 8.333333333333334 it gives double
+        System.out.println(50.0 / 6); //8.3333333333333334 it gives me double
+        System.out.println( 50d /6);  // 8.33333333333334 it gives sme double
+        System.out.println( (int) (2.5/0.5) ); // 5.0 or if we  add int inside gives us 5
+
+        System.out.println( 20 * 12);
+        System.out.println(10 /3); // numerator  -(denominator  * integer result)=3
+        System.out.println(10 -(3 * 3)); // it gives us 1
+
+
+        System.out.println(100 % 13); // it gives us 9
+        System.out.println( 45 % 8); // it gives us 8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
