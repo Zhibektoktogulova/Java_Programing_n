2654451a598eb57de7127cd4c86f742b47db7a82
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-07-31 12:49:19 -0500

    Salary Calculator  with concatenation is done

--- src/day05_operators/SalaryCalculator.java
+++ src/day05_operators/SalaryCalculator.java
@@ -26,6 +26,18 @@ public class SalaryCalculator {
         System.out.println("salaryBeforeTax = " + salaryBeforeTax);
         System.out.println("stateTax = " + stateTax);
 
+        System.out.println("Gross pay is: $" +  salaryBeforeTax);
+        System.out.println("Federal tax is: $ " + federalTax);
+        System.out.println("State tax is: $" + stateTax);
+        System.out.println("Total tax is: $" + totalTax);
+        System.out.println("Net income is: $" + salaryAfterTax);
+
+        System.out.println("---------------------------------------------");
+
+        System.out.println("Gross pay is: $" + salaryBeforeTax +
+                "\nFederal tax is: $" + federalTax +
+                "\nState tax is: $" + totalTax +
+                "\nNet income is: $" + salaryAfterTax);
 
 
 
