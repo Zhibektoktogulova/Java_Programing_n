fc80c4006148a10387ac1ecf97a9b80a1806630b
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-07-30 19:42:05 -0500

    Full Name is done

---
+++ src/day04_concatenation/FullName.java
@@ -0,0 +1,24 @@
+package day04_concatenation;
+
+public class FullName {
+
+    public static void main(String[] args) {
+
+        String firstName = "Cydeo";
+        String lastName = "School";
+
+        String fullName = firstName + " " + lastName;
+
+
+        System.out.println("Your full name is " + fullName);
+
+
+    }
+
+
+
+
+
+
+
+}
