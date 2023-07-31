037a35cf2803175da5cda6b0cf640533f4e39e4d
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-07-31 15:07:05 -0500

    Shorthand Operators is finished

---
+++ src/day05_operators/ShorthandOperators.java
@@ -0,0 +1,35 @@
+package day05_operators;
+
+public class ShorthandOperators {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        System.out.println(a); // 20
+
+        System.out.println(a); // 20
+
+        a = 40;
+
+        System.out.println(a); // 40
+
+        a = 90;
+
+        System.out.println(a); //90
+
+        System.out.println("----------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
