4b4ae08d73b7a1aab6ae75b3a1fd9e7cf776e9a7
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-08-02 15:38:18 -0500

    If and Else practice is done

---
+++ src/day06_ifStatements/IfAndElsePractice.java
@@ -0,0 +1,34 @@
+package day06_ifStatements;
+
+public class IfAndElsePractice {
+
+    public static void main(String[] args) {
+
+        int score = 75;
+
+        if (score >= 60){
+            System.out.println(" Passed");
+        }else{
+            System.out.println(" Not passed");
+        }
+        System.out.println("---------------------------");
+
+        int gradeScore = 50;
+        if (gradeScore < 49){
+            System.out.println(" Passed");
+
+        }else{
+            System.out.println(" Not Passed");
+        }
+        System.out.println("-------------------------");
+
+
+
+
+
+
+
+
+
+    }
+}
