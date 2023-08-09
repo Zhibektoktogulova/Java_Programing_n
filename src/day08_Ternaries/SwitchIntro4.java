2c46e5d7f9d34f330e8a3d00a4f417e78c2c0f05
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-08-09 15:09:27 -0500

    Switch statement 4 just finished

---
+++ src/day08_Ternaries/SwitchIntro4.java
@@ -0,0 +1,67 @@
+package day08_Ternaries;
+
+public class SwitchIntro4 {
+    public static void main(String[] args) {
+
+        int num = 11;
+        String month = "";
+
+        switch (num){
+            case 1:
+                System.out.println("December");
+                break;
+            case 2:
+                System.out.println("January");
+                break;
+            case 3:
+                System.out.println("February");
+                break;
+            case 4:
+                System.out.println("March");
+                break;
+            case 5:
+                System.out.println("April");
+                break;
+            case 6:
+                System.out.println("May");
+                break;
+            case 7:
+                System.out.println("June");
+                break;
+            case 8:
+                System.out.println("July");
+                break;
+            case 9:
+                System.out.println("August");
+                break;
+            case 10:
+                System.out.println("September");
+                break;
+            case 11:
+                System.out.println("October");
+                break;
+            case 12:
+                System.out.println("November");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
