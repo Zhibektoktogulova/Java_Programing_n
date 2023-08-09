a592cff5c5afd732f17fd72a81c6aa7e65f00022
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-08-09 12:56:04 -0500

    Ternaries just finished

--- src/day08_Ternaries/TernariesIntro.java
+++ src/day08_Ternaries/TernariesIntro.java
@@ -14,6 +14,54 @@ public class TernariesIntro {
 
         System.out.println("---------------------");
 
+
+
+        int score = 85;
+        String result = "";
+        if(score >= 60){
+            result = " Passed";
+        }else{
+            result = " Failed";
+
+        }
+        System.out.println(result);
+
+        System.out.println("--------------------------------");
+      // Ternary return
+       String result2 = (score >= 60) ? " Passed" : " Failed";
+
+        System.out.println(result2);
+
+        System.out.println("---------------------------------");
+
+        int age = 34;
+
+
+        String r;
+        if (age >= 21){
+            r =" Eligible";
+        }else{
+            r = " Not eligible";
+        }
+        System.out.println(r);
+
+        System.out.println("----------------------------------");
+       // Ternary
+
+
+        String result3 = (age >= 21) ? "Eligible" : " Not eligible";
+        System.out.println(result3);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
  /*
