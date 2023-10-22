dec75d4816d7c02b656ce1af1c6bfd5bb99c904a
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2023-10-22 17:04:45 -0500

    Format full name trim methods   just finished

--- src/day12_customMethods/FormatFullName.java
+++ src/day12_customMethods/FormatFullName.java
@@ -1,11 +1,16 @@
 package day12_customMethods;
-
+import java.util.Scanner;
 public class FormatFullName {
 
     public static void main(String[] args) {
 
-        String first_name = "cyDEO";
-        String last_name = "sCHOOL";
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String first_name = input.next().trim().replace(" ", "");
+     //trim(0 & replace() methods will make sure that the white spaces & additionals spaces between the characters will be removed
+        System.out.println("Enter your last name:");
+        String last_name = input.next().trim().replace(" ", "");
 
         first_name = first_name.substring(0, 1).toUpperCase() + first_name.substring(1).toLowerCase();
                 //"c" we call the upper "C" getting first character of first name
@@ -15,12 +20,49 @@ public class FormatFullName {
        String full_name = first_name + " " + last_name;
 
 
-        System.out.println(first_name);
-        System.out.println(last_name);
         System.out.println(full_name);
 
 
+    /*
+       This is deleted only empty space if we use ".trim();"
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter your first name:");
+        String first_name = input.next().trim();
+
+        System.out.println("Enter your last name:");
+        String last_name = input.next().trim();
+
+        first_name = first_name.substring(0, 1).toUpperCase() + first_name.substring(1).toLowerCase();
+                //"c" we call the upper "C" getting first character of first name
+
+       last_name = last_name.substring(0,1).toUpperCase() + last_name.substring(1).toLowerCase();
+
+       String full_name = first_name + " " + last_name;
+
+
+
+       If we want to empty every each words so se call the ".replace()" methods
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String first_name = input.next().trim().replace(" ", "");
+
+        System.out.println("Enter your last name:");
+        String last_name = input.next().trim().replace(" ", "");
+
+        first_name = first_name.substring(0, 1).toUpperCase() + first_name.substring(1).toLowerCase();
+                //"c" we call the upper "C" getting first character of first name
+
+       last_name = last_name.substring(0,1).toUpperCase() + last_name.substring(1).toLowerCase();
+
+       String full_name = first_name + " " + last_name;
+
+
+        System.out.println(full_name);
+
+
+     */
 
 
 
